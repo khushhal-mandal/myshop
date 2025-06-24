cd7d969f7db6f989c59bde41a94725fa98711d74
khushhal-mandal <112978822+khushhal-mandal@users.noreply.github.com>
2025-06-24 16:56:53 +0530

    Update Order.kt

--- app/src/main/java/com/example/myshoppinguserapp/domain/model/Order.kt
+++ app/src/main/java/com/example/myshoppinguserapp/domain/model/Order.kt
@@ -2,7 +2,7 @@ package com.example.myshoppinguserapp.domain.model
 
 data class Order (
     val orderId: String = "",
-    val time: Long = 0L,
+    val time: Long = System.currentTimeMillis(),
     val products: List<Cart> = emptyList(),
     val totalPrice: Int = 0,
     val shippingInfo: ShippingInfo = ShippingInfo()
